2e010c387d8d65ceeba9db55a14133486f023477
haog29 <hgagaring@yahoo.com>
2019-05-20 19:23:46 -0700

    tested CRUD routes via postman status : working

---
+++ models/burger.js
@@ -0,0 +1,75 @@
+// const { fetch, alert } = window
+
+// const getBurgers = _ => {
+//     fetch('/burger')
+//       .then(r => r.json())
+//       .then(users => {
+//         document.querySelector('#userView').innerHTML = ''
+//         users.forEach(({ id, burger_name, devoured}) => {
+//           let burgerDiv = document.createElement('div')
+//           userDiv.innerHTML = `
+//             <h3>#${id} ${burger_name}</h3>
+//             <h5>${devoured}</h5>
+          
+//             <button id="deleteBurger" data-bid="${id}">delete burger</button>
+//           `
+//           document.querySelector('#userView').append(burgerDiv)
+//         })
+//       })
+//       .catch(e => console.error(e))
+//   }
+
+//   document.querySelector('#addBurger').addEventListener('click', e => {
+//     e.preventDefault()
+//     fetch('/burger', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         burger_name: document.querySelector('#enter_text').value,
+  
+//       })
+//     })
+//       .then(_ => {
+//         alert('Burger added.')
+//         getBurgers()
+//       })
+//       .catch(e => console.error(e))
+//   })
+
+
+
+//   document.querySelector('#updateBurger').addEventListener('click', e => {
+//     e.preventDefault()
+//     let newValues = {}
+//     if (document.querySelector('#newname').value) {
+//       newValues.name = document.querySelector('#newname').value
+//     }
+//     fetch(`/burger/${document.querySelector('#bid').value}`, {
+//       method: 'PUT',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(newValues)
+//     })
+//       .then(_ => {
+//         alert('burger was updated.')
+//         getBurgers()
+//       })
+//       .catch(e => console.error(e))
+//   })
+
+
+//   document.addEventListener('click', e => {
+//     if (e.target.id === 'deleteBurger') {
+//       fetch(`/burger/${e.target.dataset.bid}`, { method: 'DELETE' })
+//         .then(_ => {
+//           alert('Burger deleted.')
+//           getBurgers()
+//         })
+//         .catch(e => console.error(e))
+//     }
+//   })
+  
+//   getBurgers()
--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,64 @@
 const db = require('../config')
 
+
+
 module.exports = app => {
   // your routes here...
+
+  
+//MAIN PAGE:
+
   app.get('/', (req, res) => { 
     return res.render('index')
   })
 
-  app.get('/burger/:id', (req, res) => {
-    db.query('SELECT * FROM burger', (e, burger) => {
-      if (e) throw e
-      res.render('', { burger })
-  })
-})
-
-
+  //CREATE:POST
   app.post('/burger', (req, res)=>{
-    db.create(req.body.burger_name, ()=> {
+    db.query('INSERT INTO burgers SET ?', req.body, e => {
+     
+      // res.send(ok)
+    // db.create(req.body, ()=> {
     res.render('burger')
   })
 })
 
+//READ:GET
+app.get('/burger', (req, res) => {
+  db.query('SELECT * FROM burgers', (e, burger) => {
+   
+    res.json(burger)
+})
+})
+
+
+//UPDATE:PUT
 app.put('/burger/:id', (req, res) => {
-  db.query('UPDATE burger SET ? WHERE ?', [req.body, { id: req.params.id }], e => {
-    if (e) throw e
+  db.query('UPDATE burgers SET ? WHERE ?', [req.body, { id: req.params.id }], e => {
+    
     res.sendStatus(200)
   })
 })
 
 
+//DELETE
+
   app.delete('/burger/:id', (req, res) => {
-    db.query('DELETE FROM burger WHERE ?', { id: req.params.id }, e => {
-      if (e) throw e
+    db.query('DELETE FROM burgerS WHERE ?', { id: req.params.id }, e => {
+      
       res.sendStatus(200)
   })
   })
+
+
+
+
+//   app.get('/burger/:id', (req, res) => {
+//     db.query('SELECT * FROM burgers', (e, burger) => {
+     
+//       res.render('', { burger })
+//   })
+// })
+
+
+
 }
